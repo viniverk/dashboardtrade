d2cc95c6b8cae667d7003fdc6230a199eb624f23
viniverk <106272264+viniverk@users.noreply.github.com>
2026-06-23 23:34:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "ID",
-  appId: "ID_DO_APP"
+
+  apiKey: "AIzaSyAvWlAUn5hzr-rWAaTZDAkVsPOJhlkzDC4",
+
+  authDomain: "tradeesportivodashboard.firebaseapp.com",
+
+  projectId: "tradeesportivodashboard",
+
+  storageBucket: "tradeesportivodashboard.firebasestorage.app",
+
+  messagingSenderId: "911731188311",
+
+  appId: "1:911731188311:web:fcdc39a0557d471fb8f912"
+
 };
 
 const app = initializeApp(firebaseConfig);
